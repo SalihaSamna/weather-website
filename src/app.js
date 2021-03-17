02a1cd4087fcfc61050f97f6194a56cb54330628
Saliha Samna <Saliha@SPERIYA1-IN-LE1.eu.trimblecorp.net>
2021-03-17 11:47:24 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const app = express()
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+
+//console.log(path.join(__dirname, '../public'))
+const pathTohtml = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+app.use(express.static(pathTohtml))
+app.set('view engine', 'hbs')
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Created by saliha samna'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Created by Saliha Samna'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Contact Us',
+        name: ' Created by Saliha Samna',
+        helptext: 'Contact saliha samna for more information'
+    })
+})
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({ Error: 'please provide address in query string' })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send(error)
+        }
+
+        forecast(latitude, longitude, (error, forecastdata) => {
+            if (error) {
+                return res.send(error)
+            }
+            res.send({
+                forecast: forecastdata,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+})
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 page',
+        errorMessage: 'Help text not found',
+        name: 'Created by Saliha Samna'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Error',
+        name: 'Created by Saliha Samna',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log("sever is up and running")
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=443154f94473dd969edac27105cae97b&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather app services', undefined)
+        } else if (body.error) {
+            callback('Unable to find the location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions + '.It is currently ' + body.current.temperature + ' degrees out,but feels like  ' + body.current.feelslike + ' degrees out')
+        }
+    })
+}
+
+module.exports = forecast
